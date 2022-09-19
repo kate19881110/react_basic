5a64b49ea402a5e1e93c3c37d3a96c6a6ced3012
kate <kate20210906@gmail.com>
2022-09-19 21:04:13 +0800

    all commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; // 類似 JQuery, 寫好的 API ,Hook，元件寫法
+import ReactDOM from "react-dom/client"; // 網頁如同白紙， ReactDOM會把你寫好的元件，畫在白紙上面
+import "./index.css";
+import Home from "./pages/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/store/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+const Context = React.createContext({
+    loading: false,
+    name: 'jack'
+})
+
+export default Context
